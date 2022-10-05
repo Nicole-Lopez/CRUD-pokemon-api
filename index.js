cbefd38ef0f2c06acea6d1b170e5d838bb9aa87e
Nicole-Lopez <ananicolelopez96@gmail.com>
2022-10-05 04:50:26 -0300

    deploy Adaptable

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const{ PORT } = process.env
 
-const port = process.env.PORT || 3001;
+const port = PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: false }).then(async() => {
   server.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`); // eslint-disable-line no-console
   });
