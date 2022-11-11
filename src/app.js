53324fe10f6c3ea39ce84e1ba6e1e12ea818c928
Nicole-Lopez <ananicolelopez96@gmail.com>
2022-11-11 02:15:38 -0300

    cors config

--- src/app.js
+++ src/app.js
@@ -4,12 +4,21 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
+const cors = require('cors')
+
 require('./db.js');
 const server = express();
-// const cors = require('cors')
 server.name = 'API';
 
-// server.use(cors())
+server.use(cors())
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(cookieParser());
